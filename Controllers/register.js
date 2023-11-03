fd8cd7b157e87f627fc8a2856ebcaa4fc6f4c536
K3WiiiN <szabokevinmark@turr.hu>
2023-11-03 14:27:30 +0100

    Regisztráció desgin, script létrehozás & megírás, tesztelés

---
+++ Controllers/register.js
@@ -0,0 +1,39 @@
+function register(){
+    let name = document.querySelector('#teljesnev');
+    let email = document.querySelector('#email');
+    let passwd = document.querySelector('#passwd');
+    let confirm = document.querySelector('#passwdconf');
+ 
+    if (name.value == "" || email.value == "" || passwd.value == "" || confirm.value == ""){
+        showMessage('Nem adtál meg minden adatot!');    }
+    else
+    {
+        if (passwd.value != confirm.value){
+            showMessage('A megadott jelszavak nem egyeznek meg!'); 
+        }
+        else
+        { 
+            console.log(name.value,email.value,passwd.value)
+            /*
+            axios.get(`${serverUrl}/users/email/eq/${email.value}`).then(res =>{
+                if (res.data.length > 0){
+                    showMessage('A megadott e-mail cím már regisztrálva van!'); 
+                }
+                else
+                {
+                    let newUser = {
+                        name: name.value,
+                        email: email.value,
+                        passwd: passwd.value
+                    }
+                    axios.post(`${serverUrl}/users`, newUser).then(res =>{
+                        alert('Sikeres regisztráció! Most már beléphetsz!');
+                        document.location.href = 'index.html';
+                    });
+                }
+            });
+          */ 
+        }
+    }
+}
+
